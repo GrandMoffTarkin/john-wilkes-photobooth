d5a0bc1c2c9d3b762f32648175619c005e9dcef7
acymbalski <acymbalski@gmail.com>
2019-01-19 17:58:50 +0000

    replace tabs with spaces

--- sample.py
+++ sample.py
@@ -89,11 +89,11 @@ def input(events):
                 (event.type == KEYDOWN and event.key == K_ESCAPE)):
             pygame.quit()
 
-			
+            
 # set variables to properly display the image on screen at right ratio
 def set_demensions(img_w, img_h):
-	# Note this only works when in booting in desktop mode. 
-	# When running in terminal, the size is not correct (it displays small). Why?
+    # Note this only works when in booting in desktop mode. 
+    # When running in terminal, the size is not correct (it displays small). Why?
 
     # connect to global vars
     global transform_y, transform_x, offset_y, offset_x
@@ -131,13 +131,13 @@ def InitFolder():
     Message = ''
 
     #check image folder existing, create if not exists
-    if not os.path.isdir(imagefolder):	
-        os.makedirs(imagefolder)	
+    if not os.path.isdir(imagefolder):    
+        os.makedirs(imagefolder)    
             
     imagefolder2 = os.path.join(imagefolder, 'images')
     if not os.path.isdir(imagefolder2):
         os.makedirs(imagefolder2)
-		
+        
 def DisplayText(fontSize, textToDisplay):
     global Numeral
     global Message
@@ -163,7 +163,7 @@ def DisplayText(fontSize, textToDisplay):
                     backgroundPicture.blit(text, textpos)
             else:
                     background.blit(text, textpos)
-				
+                
 def UpdateDisplay():
     # init global variables from main thread
     global Numeral
@@ -221,9 +221,9 @@ def UpdateDisplay():
                     background.blit(text, textpos)
     
     if(ImageShowed == True):
-    	screenPicture.blit(backgroundPicture, (0, 0))   	
+        screenPicture.blit(backgroundPicture, (0, 0))       
     else:
-    	screen.blit(background, (0, 0))
+        screen.blit(background, (0, 0))
    
     pygame.display.flip()
     return
@@ -243,17 +243,17 @@ def ShowPicture(file, delay):
     pygame.display.flip()  # update the display
     ImageShowed = True
     time.sleep(delay)
-	
+    
 # display one image on screen
-def show_image(image_path):	
-	screen.fill(pygame.Color("white")) # clear the screen	
-	img = pygame.image.load(image_path) # load the image
-	img = img.convert()	
-	set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image	
-	x = (infoObject.current_w / 2) - (img.get_width() / 2)
-	y = (infoObject.current_h / 2) - (img.get_height() / 2)
-	screen.blit(img,(x,y))
-	pygame.display.flip()
+def show_image(image_path):    
+    screen.fill(pygame.Color("white")) # clear the screen    
+    img = pygame.image.load(image_path) # load the image
+    img = img.convert()    
+    set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image    
+    x = (infoObject.current_w / 2) - (img.get_width() / 2)
+    y = (infoObject.current_h / 2) - (img.get_height() / 2)
+    screen.blit(img,(x,y))
+    pygame.display.flip()
 
 def CapturePicture():
         global imagecounter
@@ -267,28 +267,28 @@ def CapturePicture():
         global pygame
         global ImageShowed
         global CountDownPhoto
-	global BackgroundColor	
-	
-	BackgroundColor = ""
-	Numeral = ""
+    global BackgroundColor
+    
+    BackgroundColor = ""
+    Numeral = ""
         Message = ""
-	UpdateDisplay()
-	time.sleep(1)
-	CountDownPhoto = ""
-	UpdateDisplay()
-	background.fill(pygame.Color("black"))
-	screen.blit(background, (0, 0))
-	pygame.display.flip()
-	camera.start_preview()
-	BackgroundColor = "black"
+    UpdateDisplay()
+    time.sleep(1)
+    CountDownPhoto = ""
+    UpdateDisplay()
+    background.fill(pygame.Color("black"))
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+    camera.start_preview()
+    BackgroundColor = "black"
 
-	for x in range(3, -1, -1):
-                if x == 0:                        
+    for x in range(3, -1, -1):
+                if x == 0:
                         Numeral = ""
                         Message = "PRENEZ LA POSE"
-                else:                        
+                else:
                         Numeral = str(x)
-                        Message = ""                
+                        Message = ""
                 UpdateDisplay()
                 time.sleep(1)
 
@@ -305,7 +305,7 @@ def CapturePicture():
         ImageShowed = False
         return filename
     
-	
+    
 def TakePictures():
         global imagecounter
         global imagefolder
@@ -316,10 +316,10 @@ def TakePictures():
         global pygame
         global ImageShowed
         global CountDownPhoto
-	global BackgroundColor
-	global Printing
-	global PhotosPerCart
-	global TotalImageCount
+    global BackgroundColor
+    global Printing
+    global PhotosPerCart
+    global TotalImageCount
 
         input(pygame.event.get())
         CountDownPhoto = "1/3"        
@@ -339,7 +339,7 @@ def TakePictures():
         image2 = PIL.Image.open(filename2)
         image3 = PIL.Image.open(filename3)   
         TotalImageCount = TotalImageCount + 1
-	
+    
         bgimage.paste(image1, (625, 30))
         bgimage.paste(image2, (625, 410))
         bgimage.paste(image3, (55, 410))
@@ -362,8 +362,8 @@ def TakePictures():
         Printing = False
         WaitForPrintingEvent()
         Numeral = ""
-	Message = ""
-	print(Printing)
+    Message = ""
+    print(Printing)
         if Printing:
                 if (TotalImageCount <= PhotosPerCart):
                         if os.path.isfile('/home/pi/Desktop/tempprint.jpg'):
@@ -381,12 +381,12 @@ def TakePictures():
                                 if printqueuelength > 1:
                                         ShowPicture('/home/pi/Desktop/tempprint.jpg',3)
                                         conn.enablePrinter(printer_name)
-                                        Message = "Impression impossible"                
+                                        Message = "Impression impossible"
                                         UpdateDisplay()
                                         time.sleep(1)
                                 else:
                                         conn.printFile(printer_name, '/home/pi/Desktop/tempprint.jpg', "PhotoBooth", {})
-                                        time.sleep(40)            
+                                        time.sleep(40)
                 else:
                         Message = "Nous vous enverrons vos photos"
                         Numeral = ""
@@ -403,7 +403,7 @@ def MyCallback(channel):
     global Printing
     GPIO.remove_event_detect(BUTTON_PIN)
     Printing=True
-	
+    
 def WaitForPrintingEvent():
     global BackgroundColor
     global Numeral
@@ -417,31 +417,31 @@ def WaitForPrintingEvent():
     while Printing == False and countDown > 0:
         if(Printing == True):
             return
-        for event in pygame.event.get():			
-            if event.type == pygame.KEYDOWN:				
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     GPIO.remove_event_detect(BUTTON_PIN)
                     Printing = True
-                    return        
+                    return
         BackgroundColor = ""
         Numeral = str(countDown)
         Message = ""
-        UpdateDisplay()        
+        UpdateDisplay()
         countDown = countDown - 1
         time.sleep(1)
 
     GPIO.remove_event_detect(BUTTON_PIN)
         
-	
+
 def WaitForEvent():
     global pygame
     NotEvent = True
     while NotEvent:
             input_state = GPIO.input(BUTTON_PIN)
             if input_state == False:
-                    NotEvent = False			
+                    NotEvent = False
                     return
-            for event in pygame.event.get():			
+            for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             pygame.quit()
